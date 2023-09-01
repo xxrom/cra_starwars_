8981b4055183c043c8beca6cb1939bc1bba6b87f
NikitaChernyshov <xpom55@mail.ru>
2023-09-01 08:32:19 +0400

    feat: toast show on error

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import React, { memo, useEffect, useRef } from 'react';
-import { ChakraBaseProvider, extendBaseTheme } from '@chakra-ui/react';
+import {
+  ChakraBaseProvider,
+  extendBaseTheme,
+  useToast,
+} from '@chakra-ui/react';
 import chakraTheme from '@chakra-ui/theme';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Layout } from './components';
@@ -50,6 +54,7 @@ export const App = memo(() => {
   const pagesMap = useStore((store) => store.pagesMap);
   const openedPage = useStore((store) => store.openedPage);
   const { setToLS } = useLSActions();
+  const toast = useToast();
 
   // Sync data with LS
   useEffect(() => setToLS({ peopleMap }), [peopleMap, setToLS]);
@@ -64,13 +69,13 @@ export const App = memo(() => {
 
     ref.current = true; // set as visited
 
+    const state = useStore.getState();
+
     // Set my custom throwError, so I can use it inside zustand!
     useStore.setState({
       actionsError: {
-        throwError: (error: Error) => {
-          console.warn('>>> >>> custom error catcher ', error);
-          // TODO: show custom error popup
-        },
+        ...state.actionsError,
+        showToast: toast,
       },
     });
   });
--- src/hooks/useStore/errorSlice.tsx
+++ src/hooks/useStore/errorSlice.tsx
@@ -1,18 +1,40 @@
+import { Box, UseToastOptions } from '@chakra-ui/react';
 import { StateCreator } from 'zustand';
 import { StoreType, useStore } from './useStore';
 
 export type ErrorSliceType = {
   actionsError: {
+    showToast: (options: UseToastOptions) => void;
     throwError: (error: Error) => void;
   };
 };
 
 export const createErrorSlice: StateCreator<StoreType, [], [], ErrorSliceType> =
-  (_set, _get) => {
+  (_set, get) => {
     return {
       actionsError: {
+        showToast: () => {},
         throwError: (error: Error) => {
           console.warn('>>> >>> custom error catcher ', error);
+
+          const { actionsError } = get();
+
+          actionsError.showToast({
+            title: error.message || 'error',
+            duration: 10000,
+            render: ({ title }) => (
+              <Box
+                color="white"
+                p={5}
+                bg="purple.500"
+                borderRadius="6"
+                fontSize="2xl"
+                border="1px solid black"
+              >
+                {title}
+              </Box>
+            ),
+          });
           // TODO: show custom error popup
         },
       },
